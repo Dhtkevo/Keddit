1efbdaefd57294c8192b817e4eba5789b770f926
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-06 16:17:08 -0400

    Implement right side of navigation bar

--- components/NavigationBar/NavigationBar.tsx
+++ components/NavigationBar/NavigationBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavigationBar = () => {
   return (
-    <div className="bg-black h-16 border border-b-gray-50 flex justify-center items-center px-8">
+    <div className="bg-black h-16 border border-b-gray-50 flex justify-between items-center px-8 py-2">
       <div className="flex border border-blue-400">
         <img
           src="../../src/assets/Keddit_Logo_No_Text.png"
@@ -16,7 +16,7 @@ const NavigationBar = () => {
         />
       </div>
 
-      <div className="bg-gray-900 rounded-4xl h-12 w-112 flex items-center px-4 py-1 gap-2 border border-blue-500">
+      <div className="bg-gray-900 rounded-4xl h-full w-112 flex items-center px-4 py-1 gap-2 border border-blue-500">
         <i className="fa-solid fa-magnifying-glass h-fit text-white text-lg border border-red-500"></i>
         <input
           type="text"
@@ -26,6 +26,21 @@ const NavigationBar = () => {
         />
         <div className="text-white border border-red-600 rounded-full w-10 h-full flex justify-center items-center text-xs">
           X
+        </div>
+      </div>
+      <div className="text-gray-300 text-2xl flex items-center gap-6">
+        <i className="fa-brands fa-rocketchat"></i>
+        <div className="flex gap-2 items-center">
+          <i className="fa-solid fa-plus"></i>
+          <p className="text-lg">Create</p>
+        </div>
+        <i className="fa-solid fa-bell"></i>
+        <div className="rounded-full h-max border border-gray-500">
+          <img
+            src="../../src/assets/Keddit_Logo_No_Text.png"
+            alt="Profile Photo"
+            className="h-8 w-12 rounded-full object-cover"
+          />
         </div>
       </div>
     </div>
