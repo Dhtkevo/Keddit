f9934673b4e0e8718027a6e846d700305d49e462
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-07 10:06:23 -0400

    Create login interface

---
+++ components/Login/Login.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="h-screen bg-black flex">
+      <div className=" w-1/2">
+        <img
+          src="../../src/assets/Keddit_Logo_Text.png"
+          alt="Keddit Logo text"
+          className="h-full w-fit object-cover"
+        />
+      </div>
+      <div className="w-1/2 flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold mb-12 text-white">
+          Here Now, Here Always
+        </h1>
+        <button className="bg-white rounded-full px-12 py-2 flex items-center gap-2 hover:cursor-pointer hover:bg-gray-200">
+          <i className="text-lg fa-brands fa-github"></i>
+          Sign in with GitHub
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import Login from "../components/Login/Login";
 
 function App() {
   return (
     <>
-      <NavigationBar />
+      <Login />
     </>
   );
 }
