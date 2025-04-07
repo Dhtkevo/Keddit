f13b1f75b70b2f2da374492d024f77c456b14c6c
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-07 14:39:38 -0400

    Implment Post component

---
+++ components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "../Post/Post";
+
+const Home = () => {
+  return (
+    <div className="h-screen w-screen bg-gray-900 flex justify-center">
+      <div className="h-full w-1/2">
+        <Post />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import NavigationBar from "../components/NavigationBar/NavigationBar";
 import Login from "../components/Login/Login";
+import Home from "../components/Home/Home";
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <Login />
+      <Home />
     </>
   );
 }
