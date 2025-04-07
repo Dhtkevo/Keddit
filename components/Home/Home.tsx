3faadc0de32619c6b4a3367043fe91691837f73b
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-07 14:43:09 -0400

    Implement home page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,7 +4,13 @@ import Post from "../Post/Post";
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-gray-900 flex justify-center">
-      <div className="h-full w-1/2">
+      <div className="h-full w-1/2 overflow-auto">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
         <Post />
       </div>
     </div>
