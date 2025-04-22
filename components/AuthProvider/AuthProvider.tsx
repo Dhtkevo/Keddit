6c0190479c23e9398b015181a386b0092c6f76f2
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-22 19:09:06 -0400

    Add AuthProvider children prop types

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { UserType } from "../../types/types";
 
-const AuthProvider = ({ children }) => {
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<UserType | undefined>(undefined);
   return (
     <AuthContext.Provider value={{ user, setUser }}>
